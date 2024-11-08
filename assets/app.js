7f0524b98c986ea5d171b84ab6b9f3937c7b0dbe
Ayomide Aina <joytife8@gmail.com>
2024-11-08 01:00:53 +0100

    added js file, improved the html and css files

---
+++ assets/app.js
@@ -0,0 +1,110 @@
+const cart = {};
+const cartItemsContainer = document.getElementById("cartItems");
+const totalAmountContainer = document.getElementById("totalAmount");
+
+function addToCart(itemName, itemPrice) {
+    if (cart[itemName]) {
+        cart[itemName].quantity++;
+    } else {
+        cart[itemName] = { price: itemPrice, quantity: 1 };
+    }
+    updateCart();
+    updateButtonText(itemName);
+}
+
+function updateButtonText(itemName) {
+    const button = document.getElementById(`addToCart-${itemName.replace(/\s+/g, '-')}`);
+    if (cart[itemName]) {
+        button.innerHTML = `
+        <div class="quantity">
+            <span onclick="decreaseQuantity('${itemName}')">-</span>
+            ${cart[itemName].quantity}
+            <span onclick="increaseQuantity('${itemName}')">+</span>
+        </div>
+        `;
+    } else {
+        button.innerHTML = `
+            <img class="cartimage" src="assets/images/add-to-cart.png" alt="Add to Cart Icon">
+            Add to Cart
+        `;
+    }
+}
+
+function removeFromCart(itemName) {
+    if (cart[itemName]) {
+        delete cart[itemName];
+        updateCart();
+        updateButtonText(itemName);
+    }
+}
+
+function increaseQuantity(itemName) {
+    if (cart[itemName]) {
+        cart[itemName].quantity++;
+        updateCart();
+        updateButtonText(itemName);
+    }
+}
+
+function decreaseQuantity(itemName) {
+    if (cart[itemName] && cart[itemName].quantity > 1) {
+        cart[itemName].quantity--;
+    } else {
+        removeFromCart(itemName);
+    }
+    updateCart();
+    updateButtonText(itemName);
+}
+
+function updateCart() {
+    cartItemsContainer.innerHTML = '';
+    let total = 0;
+
+    Object.keys(cart).forEach(itemName => {
+        const item = cart[itemName];
+        total += item.price * item.quantity;
+
+        const listItem = document.createElement("li");
+        listItem.className = "cart-item";
+        listItem.innerHTML = `
+            <span>${itemName} - $${item.price} x ${item.quantity}</span>
+            <div class="quantity-controls">
+                <button onclick="decreaseQuantity('${itemName}')">-</button>
+                <span>${item.quantity}</span>
+                <button onclick="increaseQuantity('${itemName}')">+</button>
+                <button onclick="removeFromCart('${itemName}')" class="remove-btn">Remove</button>
+            </div>
+        `;
+        cartItemsContainer.appendChild(listItem);
+    });
+
+    totalAmountContainer.textContent = total.toFixed(2);
+}
+
+function confirmOrder() {
+    if (Object.keys(cart).length === 0) {
+        alert("Your cart is empty!");
+    } else {
+        showModal();
+    }
+}
+
+function showModal() {
+    document.getElementById("confirmationModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("confirmationModal").style.display = "none";
+}
+
+function startNewOrder() {
+    Object.keys(cart).forEach(itemName => delete cart[itemName]);
+    updateCart();
+    document.querySelectorAll('.addtocart').forEach(button => {
+        button.innerHTML = `
+            <img class="cartimage" src="assets/images/add-to-cart.png" alt="Add to Cart Icon">
+            Add to Cart
+        `;
+    });
+    closeModal();
+}
